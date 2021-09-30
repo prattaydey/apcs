90e023cc8d875a561f9a0fffc723852ca7185dd8
Prattay <pdey30@stuy.edu>
2021-09-30 12:30:31 -0400

    init setup

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,11 +1,19 @@
 //Prattay Dey
 //APCS
-//HW07 -- Hello Class
+//HW08 -- Refactor Freshie Zero
 //2021-09-30
 
 	
-public class Greet { 
+public class Greet {
+        
 	public static void main(String[] args){
-		System.out.println("No hablo queso");
+		greet("Bob");
+		greet("Bobby");
+		greet("Bobba");
 		}
+
+	public static void greet(String name){
+		System.out.println("Why, hello there, " + name + ". How do you do?");
 }
+
+
