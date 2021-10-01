757ea0ee812ec4015a42ee6aef5c63e032972aff
Prattay <pdey30@stuy.edu>
2021-10-01 12:15:52 -0400

    init setup

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,19 @@
+//Prattay Dey
+//APCS
+//HW08 -- Refactor Freshie Zero
+//2021-09-30
+
+	
+public class Greet {
+        
+	public static void main(String[] args){
+		greet("Bob");
+		greet("Bobby");
+		greet("Bobba");
+		}
+
+	public static void greet(String name){
+		System.out.println("Why, hello there, " + name + ". How do you do?");
+}
+
+
