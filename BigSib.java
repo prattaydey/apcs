8e6b6e500d1b955e4fc291916c28446bd3bc2b9e
Prattay <pdey30@stuy.edu>
2021-10-01 12:31:29 -0400

    init setup

---
+++ BigSib.java
@@ -0,0 +1,12 @@
+// Prattay Dey
+// APCS
+// HW09 -- BigSibs
+// 2021-10-01
+
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Flim");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
+    }
+}
