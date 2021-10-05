ead620f53d71ce2550bffbae41c9c91c8579283c
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-04 22:34:53 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,15 +1,16 @@
 //Prattay Dey
 //APCS
-//HW09 -- BigSibs
-//2021-10-02
+//HW10 -- BigSibs
+//2021-10-04
 
 	
 public class Greet {
-	public static void main(String[] args){
-		BigSib.greet("Bob");
-		BigSib.greet("Bobby");
-		BigSib.greet("Bobba");
-		}
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
 }
+
 
 
