992bdbfe0cacade5d9906400ca2e00c3ffe98267
Prattay <Prattay@Prashuns-iMac.local>
2021-10-04 22:29:26 -0400

    init setup

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,20 @@
+//Prattay Dey
+//APCS
+//HW09 -- BigSibs
+//2021-10-02
+
+/*
+DISCOVERIES:
+- You can call a function within a java file that is defined in another java file, as long as they are in the same directory.
+- If we aren't intending to exexute anything in a java file, a main() method is not required. 
+
+UNRESOLVED MYSTERIES:
+- Can you call a function that is defined in another file in a different directory?
+- Why is String capitalized while the other data types are in lowercase?
+*/
+
+public class BigSib {
+	public static void greet(String name){
+		System.out.println("Why, hello there, " + name + ". How do you do?");
+	}
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,15 @@
+//Prattay Dey
+//APCS
+//HW09 -- BigSibs
+//2021-10-02
+
+	
+public class Greet {
+	public static void main(String[] args){
+		BigSib.greet("Bob");
+		BigSib.greet("Bobby");
+		BigSib.greet("Bobba");
+		}
+}
+
+
