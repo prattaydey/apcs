a07b22c584f9a8dbdead7dcb6ce6b2186e8573dc
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-05 21:03:19 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,16 +1,22 @@
-//Prattay Dey
+//Team Not In Service: Prattay Dey, Nada Hameed, Fang Chen 
+//Featuring Duckies: Winnie, Ray, Cat
 //APCS
 //HW10 -- BigSibs
-//2021-10-04
+//2021-10-05
 
 	
 public class Greet {
-    public static void main( String[] args ){
-        System.out.println( BigSib.greet("Crosby") );
-        System.out.println( BigSib.greet("Stills") );
-        System.out.println( BigSib.greet("Nash") );
-    }
-}
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();
+    richard.setHelloMsg("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+
+  } //end main()
+} //end Greet
 
 
 
