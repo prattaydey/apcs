a641b64f13f7726b3c96788e167383e206757769
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-05 21:34:08 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -6,14 +6,27 @@
 
 	
 public class Greet {
-  public static void main( String[] args ) {
-    String greeting;
+	public static void main( String[] args ) {
+    	String greeting;
 
-    BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
-
-    greeting = richard.greet("freshman");
-    System.out.println(greeting);
+    	BigSib richard = new BigSib();
+    	richard.setHelloMsg("Word up");
+    	greeting = richard.greet("freshman");
+	
+	BigSib bob = new BigSib();
+	bob.setHelloMsg("Salutations");
+	greeting += bob.greet("Dr. Spaceman")
+	
+	BigSib dave = new BigSib();
+	dave.setHelloMsg("Hey ya");
+	greeting += dave.greet("Kong Fooey")
+	
+	BigSib john = new BigSib();
+	john.setHelloMsg("Sup");
+	greeting += john.greet("mom")
+	
+	
+    	System.out.println(greeting);
 
   } //end main()
 } //end Greet
