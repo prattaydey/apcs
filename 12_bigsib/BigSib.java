e9e6e880028fcfbab80ab8ee8449fb97b329f637
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-05 21:28:59 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -5,16 +5,22 @@
 
 /*
 DISCOVERIES:
-- Void is used in a function that is not intended to return anything. 
-- If you want the function to return something, replace void with the intended data type. In this case, we want to return a string.
+- Instance variables are defined outside of a method.
+- Need to state their protection (public/private) and their return type.
 
 UNRESOLVED MYSTERIES:
-- What are some of the advantages of having a void function rather than a function that returns something?
+- What is the difference between an instance variable and the type of variables we have been defining up to now?
+- What are some of the advantages of using instance variables?
 */
 
 public class BigSib {
-	public static String greet(String name){
-		String result = ("Why, hello there, " + name + ". How do you do?");
+	public static String helloMsg; // instance variable helloMsg
+	public static void setHelloMsg (String newMsg) { // method to set helloMsg value
+		helloMsg = newMsg;
+	} 
+	
+	public static String greet(String subject){
+		String result = (helloMsg + subject);
 		return result;
 	}
 }
