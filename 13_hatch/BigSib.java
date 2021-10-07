67a07ed1a7d3ab5a79a3472dcf9a1c1084f5b083
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-06 20:37:26 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,7 +1,7 @@
 //Prattay Dey and Sophia Eiden
 //APCS
 //HW12 -- BigSib Instance Variables
-//2021-10-05
+//2021-10-06
 
 /*
 DISCOVERIES:
@@ -14,19 +14,20 @@ UNRESOLVED MYSTERIES:
 */
 
 public class BigSib {
-	private static String helloMsg; // instance variable helloMsg
+	private static String helloMsg; //instance var helloMsg
 	
-	public BigSib(String newMsg){
+	public BigSib(String newMsg){ //creates constructor
 		helloMsg = newMsg
 	} 
-	
-	public void setHelloMsg(String newMsg){
-		BigSib helloMsg = new BigSib(newMsg);
-	}
 		
+	public void setHelloMsg(String newMsg){ 
+		BigSib helloMsg
+		helloMsg = new BigSib(newMsg); //invokes constructor using "new", initializes instance var helloMsg
+	}
 	
 	public static String greet(String subject){
 		String result = (helloMsg + " " + subject);
 		return result;
 	}
+	
 }
