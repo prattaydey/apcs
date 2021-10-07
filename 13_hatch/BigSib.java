d2dbe5cadeb14005fb508fda8707d6e7214c9f33
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-06 21:58:52 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,22 +1,22 @@
-//Prattay Dey and Sophia Eiden
+//Team Elder Gods: Prattay Dey and Sophia Eiden
 //APCS
 //HW12 -- BigSib Instance Variables
 //2021-10-06
 
 /*
 DISCOVERIES:
-- 
-- 
+- Constructors can have variable defs inside them
+- You can call on the constructor to create an object in the class, using keyword "new"
+- Constructors can have multiple parameters
 
 Q/C/C:
-- 
-- 
+- What are the benefits of using a constructor compared to a method? 
 */
 
 public class BigSib {
 	String helloMsg; 
 	
-	public BigSib(String newMsg){ //creates constructor
+	public BigSib(String newMsg){ // creates constructor
 		helloMsg = newMsg;
 	} 
 	
