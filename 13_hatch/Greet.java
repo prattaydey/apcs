8a0689cb1f0f82afcbe6adac37791e3455d66d8e
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-06 20:53:04 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -10,25 +10,21 @@ public class Greet {
 
     	BigSib richard = new BigSib("Word Up");
     	greeting = richard.greet("freshman");
-	
 	System.out.println(greeting);
 	
-	BigSib bob = new BigSib();
-	bob.setHelloMsg("Salutations");
+		
+	richard.setHelloMsg("Salutations");
 	greeting = bob.greet("Dr. Spaceman");
-	
 	System.out.println(greeting);
 	
-	BigSib dave = new BigSib();
-	dave.setHelloMsg("Hey ya");
+	
+	richard.setHelloMsg("Hey ya");
 	greeting = dave.greet("Kong Fooey");
-	
 	System.out.println(greeting);
 	
-	BigSib john = new BigSib();
-	john.setHelloMsg("Sup");
+		
+	richard.setHelloMsg("Sup");
 	greeting = john.greet("mom");
-	
 	System.out.println(greeting);
 
   } //end main()
