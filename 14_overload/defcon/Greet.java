f484628194b454574af7e5bfb5a11026961a4113
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-07 21:09:11 -0400

    Update Greet.java

--- 14_overload/defcon/Greet.java
+++ 14_overload/defcon/Greet.java
@@ -3,7 +3,18 @@
 //HW14-- Customize Your Creation
 //2021-10-07
 
-	
+/*
+DISCOVERIES:
+Single quotations can only take a single character in java.
+A contructor with no params still run and can be useful in many cases.
+The difference between the Greet files is that one of them takes an argument
+while creating the new instance of BigSib
+
+QCC:
+Can a constructor take multiple arguments, an array of arguments?
+What other parameters could we include to make the code more efficient?
+*/
+
 public class Greet{
   public static void main(String[] args){
     String greeting;
