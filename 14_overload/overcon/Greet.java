ea6199de583ba1f6d692d92aa964c24306fa5a14
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-07 20:52:40 -0400

    Update Greet.java

---
+++ 14_overload/overcon/Greet.java
@@ -0,0 +1,22 @@
+//HPN- Hugo Jenkins, Boary, Nada Hameed, Ray, Prattay Dey, Winnie
+//APCS
+//HW14 -- Customize Your Creation
+//2021-10-07
+
+public class Greet{
+  public static void main(String[] args){
+    String greeting;
+    BigSib richard = new BigSib();
+    BigSib grizz = new BigSib();
+    BigSib dotCom = new BigSib();
+    BigSib tracy = new BigSib();
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+    greeting = tracy.greet("Dr. Spaceman");
+    System.out.println(greeting);
+    greeting = grizz.greet("Kong Fooey");
+    System.out.println(greeting);
+    greeting = dotCom.greet("mom");
+    System.out.println(greeting);
+  }
+}
