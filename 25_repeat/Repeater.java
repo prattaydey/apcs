43979cbe0f4719134bc882970d6e4ee3caca2afb
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-26 21:31:54 -0400

    Update Repeater.java

--- 25_repeat/Repeater.java
+++ 25_repeat/Repeater.java
@@ -15,10 +15,10 @@ QCC:
 
 public class Fence{
   public static String fenceW(int fences){
-    int built = 1;
+    int built = 1; // built serves as counter 
     String result = "|";
-    while(built<fences){
-      result+="--|";
+    while (built<fences){
+      result += "--|";
       built++;
     }
     return result;
@@ -26,10 +26,12 @@ public class Fence{
   public static String fenceR(int fences){
     if (fences==1){
       return "|";
-    }else {
-      return fenceR(fences-1)+"--|";
+    }
+    else {
+      return fenceR(fences-1) + "--|";
     }
   }
+  
   public static void main(String[] args){
     System.out.println(fenceW(4));
     System.out.println(fenceR(4));
