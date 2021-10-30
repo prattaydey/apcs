b210e62890cf76cedbc72a129e8ff5dd85897c70
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-30 14:14:22 -0400

    Update and rename foo to foo.java

---
+++ cb/string-1/foo.java
@@ -0,0 +1,19 @@
+// helloName
+// Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+public String helloName(String name) {
+  return "Hello " + name + "!";
+}
+
+// makeAbba
+// Given two strings, a and b, return the result of putting them together in the order abba.
+public String makeAbba(String a, String b) {
+  return a + b + b + a;
+}
+
+public static void main(String[] args) {
+  System.out.println(helloName("Bob")) // "Hello Bob!"
+  System.out.println(helloName("Alice")) // "Hello Alice!"
+  System.out.println(helloName("X")) // "Hello X!"
+  System.out.println(makeAbba("Hi", "Bye")) // "HiByeByeHi"
+  System.out.println(makeAbba("Yo", "Alice")) // "YoAliceAliceYo"
+  System.out.println(makeAbba("What", "Up")) // "WhatUpUpWhat"
