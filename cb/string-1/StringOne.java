7a3d5f3af23283df1066aed62c8e747be91d5f23
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-30 21:06:41 -0400

    Update StringOne.java

--- cb/string-1/StringOne.java
+++ cb/string-1/StringOne.java
@@ -7,13 +7,13 @@ public class StringOne {
   
   // helloName
   // Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
-  public String helloName(String name) {
+  public static String helloName(String name) {
     return "Hello " + name + "!";
   }
 
   // makeAbba
   // Given two strings, a and b, return the result of putting them together in the order abba.
-  public String makeAbba(String a, String b) {
+  public static String makeAbba(String a, String b) {
     return a + b + b + a;
   }
 
