ab4ea4829db9fac8e39801b1a74b6bafe71e763f
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-30 21:08:13 -0400

    Update StringTwo.java

--- cb/string-2/StringTwo.java
+++ cb/string-2/StringTwo.java
@@ -19,7 +19,7 @@ public class StringTwo {
 
   // countHi
   // Return the number of times that the string "hi" appears anywhere in the given string.
-  public int countHi(String str) {
+  public static int countHi(String str) {
     int counter = 0;
     for (int i = 0; i < (str.length()-1); i++) {
       if (str.substring(i, i + 2).equals("hi")){
