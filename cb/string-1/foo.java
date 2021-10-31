0fb2e92552efab5efb2b0f1a251469d62bdad8af
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2021-10-30 20:55:55 -0400

    Update foo.java

--- cb/string-1/foo.java
+++ cb/string-1/foo.java
@@ -1,3 +1,8 @@
+/* 
+Prattay Dey
+APCS
+*/
+
 // helloName
 // Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
 public String helloName(String name) {
@@ -14,6 +19,7 @@ public static void main(String[] args) {
   System.out.println(helloName("Bob")) // "Hello Bob!"
   System.out.println(helloName("Alice")) // "Hello Alice!"
   System.out.println(helloName("X")) // "Hello X!"
+    
   System.out.println(makeAbba("Hi", "Bye")) // "HiByeByeHi"
   System.out.println(makeAbba("Yo", "Alice")) // "YoAliceAliceYo"
   System.out.println(makeAbba("What", "Up")) // "WhatUpUpWhat"
