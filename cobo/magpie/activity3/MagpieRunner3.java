7f4c76900ce34c50e953fc9e0510a3f6d7a8f818
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2022-01-12 21:40:25 -0500

    Update MagpieRunner3.java

--- cobo/magpie/activity3/MagpieRunner3.java
+++ cobo/magpie/activity3/MagpieRunner3.java
@@ -1,3 +1,17 @@
+// Peanut Butter and Nutella - Prattay Dey + Winnie, Brian Li + Robert, Nafiz Labib + Martha
+// APCS pd6
+// HW56 -- Turing Training Wheels
+// 2022-01-12
+// time spent: 1.0 hours
+
+/*
+DISCO:
+- The else-if statement that is utilized is the one with greater priority.
+- trim() gets rid of leading and lagging whitespace.
+QCC:
+- Is there a way to refactor the code so that it isn't so "long"?
+*/
+
 import java.util.Scanner;
 
 /**
@@ -14,11 +28,11 @@ public class MagpieRunner3
 	public static void main(String[] args)
 	{
 		Magpie3 maggie = new Magpie3();
-		
+
 		System.out.println (maggie.getGreeting());
 		Scanner in = new Scanner (System.in);
 		String statement = in.nextLine();
-		
+
 		while (!statement.equals("Bye"))
 		{
 			System.out.println (maggie.getResponse(statement));
