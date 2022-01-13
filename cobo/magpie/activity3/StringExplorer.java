e03002492724922b13bde0ea5f1bcc95c1ce90a3
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2022-01-12 21:41:29 -0500

    Update StringExplorer.java

--- cobo/magpie/activity3/StringExplorer.java
+++ cobo/magpie/activity3/StringExplorer.java
@@ -1,6 +1,20 @@
+// Peanut Butter and Nutella - Prattay Dey + Winnie, Brian Li + Robert, Nafiz Labib + Martha
+// APCS pd6
+// HW56 -- Turing Training Wheels
+// 2022-01-12
+// time spent: 1.0 hours
+
+/*
+DISCO:
+- The else-if statement that is utilized is the one with greater priority.
+- trim() gets rid of leading and lagging whitespace.
+QCC:
+- Is there a way to refactor the code so that it isn't so "long"?
+*/
+
 /**
- * A program to allow students to try out different 
- * String methods. 
+ * A program to allow students to try out different
+ * String methods.
  * @author Laurie White
  * @version April 2012
  */
@@ -10,18 +24,24 @@ public class StringExplorer
 	public static void main(String[] args)
 	{
 		String sample = "The quick brown fox jumped over the lazy dog.";
-		
+
 		//  Demonstrate the indexOf method.
 		int position = sample.indexOf("quick");
 		System.out.println ("sample.indexOf(\"quick\") = " + position);
-		
+
 		//  Demonstrate the toLowerCase method.
 		String lowerCase = sample.toLowerCase();
 		System.out.println ("sample.toLowerCase() = " + lowerCase);
 		System.out.println ("After toLowerCase(), sample = " + sample);
-		
-		//  Try other methods here:
 
+		//  Try other methods here:
+		int notFoundPsn = sample.indexOf("slow");
+		System.out.println("sample.indexOf(\"slow\") = " + notFoundPsn);
+
+		int hePosition = sample.indexOf("he");
+		int overloadPosition = sample.indexOf("he", 3);
+		System.out.println("sample.indexOf(\"he\") = " + hePosition);
+		System.out.println("sample.indexOf(\"he\", 3) = " + overloadPosition);
 	}
 
 }
