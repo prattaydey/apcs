c27f0cbaf3e77420a4a73d7b2343c33fe3ad300d
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2022-03-20 21:14:27 -0400

    Update CardTester.java

--- cobo/elevens/Activity1 Starter Code/CardTester.java
+++ cobo/elevens/Activity1 Starter Code/CardTester.java
@@ -8,6 +8,19 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card Kartik = new Card ("First", "Hearts", 10);
+		Card Nafiz = new Card ("First", "Hearts", 10);
+		Card Prattay = new Card ("Second", "Spades", 8);
+
+		System.out.println(Kartik.suit());
+		System.out.println(Kartik.rank());
+		System.out.println(Kartik.pointValue());
+
+		System.out.println(Kartik.matches(Nafiz));
+		System.out.println(Kartik.matches(Prattay));
+
+		System.out.println(Prattay.toString());
+
+
 	}
 }
