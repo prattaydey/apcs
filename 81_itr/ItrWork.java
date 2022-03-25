77bc4d35f50466fc9b479f02921b2d5d72ffb46a
Prattay <Prattay@Prashuns-iMac.local>
2022-03-24 20:06:46 -0400

    fin

--- 81_itr/ItrWork.java
+++ 81_itr/ItrWork.java
@@ -1,3 +1,12 @@
+// Team McDonalds$24MScam: Prattay Dey, Joshua Gao, Faiza Huda
+// Ducks : Winnie, Batman, Truthful Tom, Huebert
+// APCS pd08
+// HW81 -- Thank You, Next
+// 2022-03-24
+// time spent: 0.5 hr
+// KtS consumed: 1
+
+
 /***
  * class ItrWork
  *  SKELETON
@@ -6,13 +15,21 @@
 
 /***
     DISCO
+    - When using iterator and traversing a list, it is necessary to typecast the Integer object to an int first. Else an error with incompatible types will appear.
 
     QCC
+    - When removing evens, why can't we remove from List L directly instead of the iterator?
+      Throws a "ConcurrentModificationException"
 
     SUMMARY THE FIRST:
+    hasNext(): checks to see if there is a next element in the list
+    next(): returns the next element in the list
+    remove(): removes the last element in the list
 
     SUMMARY THE SECOND:
-
+    hasNext(): returns true if iteration has more elements after the current index
+    next(): returns the next element in the list
+    remove(): removes the current index or element that iterator is on, then calls next()
 **/
 
 
@@ -30,7 +47,12 @@ public class ItrWork
   public static boolean foundA( Integer key,
                                 List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    for ( int n : L ){
+      if (n == key){
+        return true;
+      }
+    }
+    return false;
   }
 
   //explicitly using an iterator
@@ -38,21 +60,47 @@ public class ItrWork
   public static boolean foundB( Integer key,
                                 List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    Iterator it = L.iterator(); // creates Iterator Object for given List
+
+    while ( it.hasNext() ){
+      int n = (int) it.next(); // typecast Integer Object into int value
+
+      if (n == key){
+        return true;
+      }
+    }
+    return false;
   }
 
   //using FOREACH loop
   //returns a list containing the odd numbers in L
   public static List<Integer> oddsA( List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    List<Integer> result = new ArrayList();
+
+    for ( int n : L ){
+      if (n % 2 == 1){
+        result.add(n);
+      }
+    }
+    return result;
   }
 
   //explicitly using an iterator
   //returns a list containing the odd numbers in L
   public static List<Integer> oddsB( List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    List<Integer> result = new ArrayList();
+
+    Iterator it = L.iterator(); // creates Iterator Object for given List
+
+    while ( it.hasNext() ){
+      int n = (int) it.next();
+      if (n % 2 == 1){
+        result.add(n);
+      }
+    }
+    return result;
   }
 
 
@@ -60,18 +108,25 @@ public class ItrWork
   //modifies L s.t. it contains no evens
   public static void removeEvens( List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    Iterator it = L.iterator(); // creates Iterator Object for given List
+
+    while ( it.hasNext() ){
+      int n = (int) it.next(); // traverses list
+
+      if (n % 2 == 0){
+        it.remove();
+      }
+    }
   }
 
 
   public static void main( String [] args )
   {
-    /*~~~~~~~~~~~~~~~m~o~v~e~~m~e~~d~o~w~n~~~~~~~~~~~~~~
 
     //var type: List   obj type: ?
-    List<Integer> L =
+    List<Integer> L = new ArrayList();
 
-    for( int i = 0; i < 10; i++ )
+    for ( int i = 0; i < 10; i++ )
       L.add(i);
 
 
@@ -102,6 +157,7 @@ public class ItrWork
     System.out.println("\nTesting removeEvens...");
     removeEvens(L);
     for( int n : L ) System.out.println(n);
+    /*~~~~~~~~~~~~~~~m~o~v~e~~m~e~~d~o~w~n~~~~~~~~~~~~~~
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
   }//end main
