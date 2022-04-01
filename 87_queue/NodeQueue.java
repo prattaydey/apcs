13e0c41edd361897618912e89be6a8496e2e6289
Prattay <pdey30@stuy.edu>
2022-04-01 14:06:13 -0400

    init

---
+++ 87_queue/NodeQueue.java
@@ -0,0 +1,34 @@
+public class NodeQueue<QUASAR>
+{
+  Node _head;
+  Node _tail;
+
+  public NodeQueue(){
+    _head = null;
+    _tail = null;
+  }
+  //~~~~~~~~~~~~~~~~~~begin AP subset~~~~~~~~~~~~~~~~~~
+  //means of removing an element from collection:
+  //Dequeues and returns the first element of the queue.
+  public QUASAR dequeue(){
+
+  }
+
+  //means of adding an element to collection:
+  //Enqueue an element onto the back of this queue.
+  public void enqueue( QUASAR x ){
+
+  }
+
+  //Returns true if this queue is empty, otherwise returns false.
+  public boolean isEmpty(){
+
+  }
+
+  //Returns the first element of the queue without dequeuing it.
+  public QUASAR peekFront(){
+
+  }
+  //~~~~~~~~~~~~~~~~~~~end AP subset~~~~~~~~~~~~~~~~~~~
+
+}//end interface Queue
