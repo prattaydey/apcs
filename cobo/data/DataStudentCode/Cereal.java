5978d02eafd8e665101100496387c46d9d37014d
Prattay <pdey30@stuy.edu>
2022-04-05 14:03:07 -0400

    class

---
+++ cobo/data/DataStudentCode/Cereal.java
@@ -0,0 +1,71 @@
+public class Cereal
+{
+  private String _name;
+  private String _type;
+  private int _calories;
+  private int _protein;
+  private int _fat;
+  private int _sodium;
+  private double _fiber;
+  private double _carbs;
+  private int _sugar;
+  private int _potassium;
+  private int _vitamins;
+  private int _shelf;
+  private double _weight;
+  private double _cups;
+  private double _rating;
+
+  public Cereal(String name, String type, int calories, int protein, int fat, int sodium, double fiber, double carbs, int sugar, int potassium, int vitamins, int shelf, double weight, double cups, double rating)
+  {
+    _name = name;
+    _type = type;
+    _calories = calories;
+    _protein = protein;
+    _fat = fat;
+    _sodium = sodium;
+    _fiber = fiber;
+    _carbs = carbs;
+    _sugar = sugar;
+    _potassium = potassium;
+    _vitamins = vitamins;
+    _shelf = shelf;
+    _weight = weight;
+    _cups = cups;
+    _rating = rating;
+  }
+
+  public String getName()
+  {
+    return _name;
+  }
+
+  public String getType()
+  {
+    return _type;
+  }
+
+  public int getCalories()
+  {
+    return _calories;
+  }
+
+  public int getProtein()
+  {
+    return _protein;
+  }
+
+  public int getFat()
+  { return _fat; }
+
+  public String getName()
+  {
+
+  }
+
+  public String getName()
+  {
+
+  }
+
+}
